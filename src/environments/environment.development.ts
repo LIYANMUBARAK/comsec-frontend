2a4085698b802b8abeab5ef2975c321e2beb136d
shiyas <shiyastofficial@gmail.com>
2025-10-23 12:46:43 +0530

    url update

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,7 +2,8 @@ export const environment = {
   production: false,
   //  baseURL: "http://13.201.191.186:3000/"
   //  baseURL: "http://localhost:3000/"
-      baseURL: "http://65.0.45.189:3000/"
+      // baseURL: "http://65.0.45.189:3000/"
+      baseURL: "https://comsec-backend-2.vercel.app/",
     // baseURL: '/api/'
 
 };
