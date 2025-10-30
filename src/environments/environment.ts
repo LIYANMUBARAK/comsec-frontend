a6f29a3bf6683d6b37c94335c0001cd8363ba365
shiyas03 <108790858+shiyas03@users.noreply.github.com>
2025-10-30 14:07:11 +0530

    Toggle baseURL for API endpoint configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@ export const environment = {
     //  baseURL: "http://localhost:3000/"
     // baseURL: "http://65.0.45.189:3000/"
     // baseURL: "https://cors-anywhere.herokuapp.com/http://65.0.45.189:3000/",
-    baseURL: "https://comsec-backend-2.vercel.app/",
-    // baseURL: '/api/'
+    // baseURL: "https://comsec-backend-2.vercel.app/",
+    baseURL: '/api/'
     
     
 };
